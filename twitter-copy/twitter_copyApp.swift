b8fae1c11d2dfec6a0af6925e945afe9389e6f1b
New Author Name <739856334zhou@gmail.com>
2023-04-09 12:55:13 +0800

    Initial Commit

---
+++ twitter-copy/twitter_copyApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_copyApp.swift
+//  twitter-copy
+//
+//  Created by zhouwen on 2023/4/9.
+//
+
+import SwiftUI
+
+@main
+struct twitter_copyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

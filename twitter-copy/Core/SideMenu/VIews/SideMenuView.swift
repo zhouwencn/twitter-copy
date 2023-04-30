b2ab18ed21a45009d18aafe2768a40d129d225bb
New Author Name <739856334zhou@gmail.com>
2023-04-30 23:39:06 +0800

    修改

--- twitter-copy/Core/SideMenu/VIews/SideMenuView.swift
+++ twitter-copy/Core/SideMenu/VIews/SideMenuView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
+
+
 struct SideMenuView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("1234")
+        }
     }
 }
 
